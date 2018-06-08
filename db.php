26dd6c9ca78ccf61f6d92730a46ad9db3d2f153c
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-08 16:54:58 +0300

    Update db.php

--- db.php
+++ db.php
@@ -15,4 +15,7 @@ if ($_SERVER['SERVER_NAME'] != "localhost") {
 	$database = mysqli_connect($host, $username, $password, $db);
 }
 
+mysqli_query($database, "SET NAMES 'utf8' COLLATE 'utf8_general_ci'");
+mysqli_query($database, "SET CHARACTER SET 'utf8'");
+
 ?>
