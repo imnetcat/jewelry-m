57a13ece93fc762c315284810af852a59029fa95
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-08 15:20:17 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,22 +1,53 @@
 <!DOCTYPE html>
 <html><head>
   <link rel="stylesheet" href="styles.css" type="text/css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script async="">
-			$( () => {
-				var client_w = screen.width;
-        console.log(client_w);
-        if(client_w < 1024){
-          $('#container').css({
-            "marginLeft": "1vw",
-            "width": "96vw"
-          })
-        }
-
-			});
+    $( () => {
+      var client_w = screen.width;
+      console.log(client_w);
+      if(client_w < 1024){
+        $('#container').css({
+          "marginLeft": "1vw",
+          "width": "96vw"
+        })
+      }
+    });
+  </script>
+  <script async>
+    $( () => {
+      $('#new').click( () => {
+	$.ajax({
+          type: "POST",
+	  url: "actions.php",
+	  data: {
+	    action: 'new',
+	    id: $('#new_id').value,
+	    filter_1: $('#new_filter_1').value,
+	    filter_2: $('#new_filter_2').value,
+	    filter_3: $('#new_filter_3').value,
+	    filter_4: $('#new_filter_4').value,
+	    filter_5: $('#new_filter_5').value,
+	    filter_5: $('#new_filter_5').value,
+	    description: $('#new_description').value,
+	  },
+          success: function(data){
+	    $('#info').html(data);
+          }
+        });
+      });
+    });
   </script>
 </head>
 <body>
+<button id="new">Add new<button><br>
+<input id="new_id"><br>
+<input id="new_filter_1"><br>
+<input id="new_filter_2"><br>
+<input id="new_filter_3"><br>
+<input id="new_filter_4"><br>
+<input id="new_filter_5"><br>
+<input id="new_description"><br>
 <div id="container"><br><div id="info"></div><br><br><br>
     <? 
     require_once "../db.php";
