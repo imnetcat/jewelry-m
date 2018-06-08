f306ec30b2eef8dd10625418f012d515a9f79271
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-08 15:42:06 +0300

    Update actions.php

--- admin/actions.php
+++ admin/actions.php
@@ -3,7 +3,7 @@ require_once "functions.php";
 
 
 switch ($_POST['action']){
-      case 'new':
+      case 'add_new':
       $image = $_POST['image'];
       $filter_1 = $_POST['filter_1'];
       $filter_2 = $_POST['filter_2'];
@@ -11,7 +11,7 @@ switch ($_POST['action']){
       $filter_4 = $_POST['filter_4'];
       $filter_5 = $_POST['filter_5'];
       $description = $_POST['description'];
-      echo new($image, $filter_1, $filter_2, $filter_3, $filter_4, $filter_5, $description);
+      echo add_new($image, $filter_1, $filter_2, $filter_3, $filter_4, $filter_5, $description);
       break;
 };
 ?>
