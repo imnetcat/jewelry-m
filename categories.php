06234bf7ab313bfbf3a5365c27ca2dd4a2579d13
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-08 13:48:08 +0300

    Update categories.php

--- categories.php
+++ categories.php
@@ -1,5 +1,13 @@
 <div id="categories">
-  <div class="row">
+  <div class="row absolute">
+    <div class="category"><a href="/jewelry/categories/rings/page.php"><img src=""></a></div>
+    <div class="category"><a><img></a></div>
+    <div class="category"><a><img></a></div>
+    <div class="category"><a><img></a></div>
+    <div class="category"><a><img></a></div>
+    <div class="category"><a><img></a></div>
+  </div>
+  <div class="row relative">
     <div class="category"><a href="/jewelry/categories/rings/page.php"><img src=""></a></div>
     <div class="category"><a><img></a></div>
     <div class="category"><a><img></a></div>
