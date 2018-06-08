ffb19ea57fa547080796ed4fe1b8a78592b26960
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-08 13:33:00 +0300

    Create categories.php

---
+++ categories.php
@@ -0,0 +1,10 @@
+<div id="categories">
+  <div class="row">
+    <div class="category"><a href="/jewelry/categories/rings/page.php"><img src=""></a></div>
+    <div class="category"><a><img></a></div>
+    <div class="category"><a><img></a></div>
+    <div class="category"><a><img></a></div>
+    <div class="category"><a><img></a></div>
+    <div class="category"><a><img></a></div>
+  </div>
+</div>
