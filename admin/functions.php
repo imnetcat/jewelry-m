8ace3b12cbbc989d8b368df4f733faf5b45350d9
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-08 15:48:06 +0300

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -11,7 +11,7 @@ function add_new($image, $filter_1, $filter_2, $filter_3, $filter_4, $filter_5, 
   }
   $query = "INSERT INTO items (id, image, filter_1, filter_2, filter_3, filter_4, filter_5, description) VALUES (?,?,?,?,?,?,?,?)";
   $stmt = mysqli_prepare($database, $query);
-  mysqli_stmt_bind_param($stmt, "isssssss", $new_id, $image, $filter_1, $filter_2, $filter_3, $filter_4, $filter_5, $description););
+  mysqli_stmt_bind_param($stmt, "isssssss", $new_id, $image, $filter_1, $filter_2, $filter_3, $filter_4, $filter_5, $description);
   if(mysqli_stmt_execute($stmt)){
     mysqli_stmt_close($stmt);
     return "Изображение успешно добавлено!";
