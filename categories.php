0c9e4118d7e249894fb17570e975e7be96f69ff7
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-08 13:49:17 +0300

    Update categories.php

--- categories.php
+++ categories.php
@@ -8,11 +8,11 @@
     <div class="category"><a><img></a></div>
   </div>
   <div class="row relative">
-    <div class="category"><a href="/jewelry/categories/rings/page.php"><img src=""></a></div>
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
+    <div class="category">Кольца</div>
+    <div class="category">Браслеты</div>
+    <div class="category">Ожерелья</div>
+    <div class="category">Серьги</div>
+    <div class="category">Еще что-то</div>
+    <div class="category">Другое</div>
   </div>
 </div>
