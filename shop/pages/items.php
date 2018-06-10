c014239cf8aa209bb1953a89a81631a1210697fe
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-10 21:53:32 +0300

    Update items.php

--- shop/pages/items.php
+++ shop/pages/items.php
@@ -1,29 +1,31 @@
 <div id="categories">
-  <div class="btn left"><big><</big></div>
   <div class="row">
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
-    <div class="category"><a><img></a></div>
-  </div>
-  <div class="btn right"><big>></big></div>
+    <div class="btn left"><big>&lt;</big></div>
+    <div id="row">
+      <div class="category"><a><img></a></div>
+      <div class="category"><a><img></a></div>
+      <div class="category"><a><img></a></div>
+      <div class="category"><a><img></a></div>
+      <div class="category"><a><img></a></div>
+      <div class="category"><a><img></a></div>
+      <div class="category"><a><img></a></div>
+      <div class="category"><a><img></a></div>
+    </div>
+    <div class="btn right"><big>&gt;</big></div>
 </div>
 <div id="filters">
   <div id="to_types">
     <div id="types">
       <div>
-        <div value="rings">rings <label></div>
-        <div value="bracelets">bracelets <label></div>
-        <div value="necklaces">necklaces <label></div>
-        <div value="earrings">earrings <label></div>
-        <div value="pendantS">pendants <label></div>
-        <div value="other">other <label></div>
+        <div value="rings">rings <label></label></div>
+        <div value="bracelets">bracelets <label></label></div>
+        <div value="necklaces">necklaces <label></label></div>
+        <div value="earrings">earrings <label></label></div>
+        <div value="pendants">pendants <label></label></div>
+        <div value="other">other <label></label></div>
       </div>
       <button>Close</button>
     </div>
   </div>
  </div>
+</div>
