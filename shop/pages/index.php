3a8c2b9394d103059873b2e9b71c0a4d21893a09
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-10 22:36:06 +0300

    Update index.php

--- shop/pages/index.php
+++ shop/pages/index.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html><head>
-  <style>
-    <? include "styless.css"; ?>
-  </style>
+  <link rel="stylesheet" href="style.css" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script async>
 			$( () => {
