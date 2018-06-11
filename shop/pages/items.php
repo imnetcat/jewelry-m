03eb315d4cfdd6ba6854138edb50258e95397d88
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-11 19:10:13 +0300

    Update items.php

--- shop/pages/items.php
+++ shop/pages/items.php
@@ -4,20 +4,6 @@
     <div id="row">
     </div>
     <div class="btn right"><big>&gt;</big></div>
-</div>
-<div id="filters">
-  <div id="to_types">
-    <div id="types">
-      <div>
-        <div value="rings">rings <label></label></div>
-        <div value="bracelets">bracelets <label></label></div>
-        <div value="necklaces">necklaces <label></label></div>
-        <div value="earrings">earrings <label></label></div>
-        <div value="pendants">pendants <label></label></div>
-        <div value="other">other <label></label></div>
-      </div>
-      <button>Close</button>
-    </div>
   </div>
- </div>
 </div>
+<? include "filters.html"; ?>
