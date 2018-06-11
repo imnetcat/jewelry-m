d28daccacf5c5756e0d0232a052adc19ee8b4eb8
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-11 21:13:35 +0300

    Update filters.js

--- shop/pages/filters.js
+++ shop/pages/filters.js
@@ -2,11 +2,21 @@ $( () => {
   $('#to_types').click( () => {
     $('#types').css({
       visibility: "visible",
+      z-index: 100
+    });
+    $('#filters').css({
+      visibility: "hidden",
+      z-index: 99
     });
   });
   $('.fil-close').click( () => {
     $('#types').css({
       visibility: "hidden",
+      z-index: 99
+    });
+    $('#filters').css({
+      visibility: "visible",
+      z-index: 100
     });
   });
 });
