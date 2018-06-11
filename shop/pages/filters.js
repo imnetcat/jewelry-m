2edd0366776d8681b97f1b3ed42ae0225e51750d
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-11 20:17:59 +0300

    Create filters.js

---
+++ shop/pages/filters.js
@@ -0,0 +1,12 @@
+$( () => {
+  $('#to_types').click( () => {
+    $('#types').animate({
+      visibility: visible;
+    }, 500);
+  });
+  $('.fil-close').click( () => {
+    $('#types'.animate({
+      visibility: hidden;
+    }, 500);
+  });
+   
