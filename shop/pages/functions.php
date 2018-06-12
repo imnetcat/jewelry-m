a8b971b59deedfe220624a055620ec342d6e4f54
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-12 17:41:14 +0300

    Update functions.php

--- shop/pages/functions.php
+++ shop/pages/functions.php
@@ -1,12 +1,14 @@
 <?
 function get_items($database, $filters){
-  list($type_1, $type_2, $type_3, $type_4, $type_5, $type_6, $type_7) = explode(",", $filters->types);
-  $query ="SELECT id FROM items WHERE type = $type_1 OR $type_2 OR $type_3 OR $type_4 OR $type_5 OR $type_6 OR $type_7";
-  if($result = mysqli_query($database, $query)){
-    $id = array();
-    while ($row = mysqli_fetch_row($result)) {
-      for( $n = 0; $n < count($row); $n++ ){
-        return $id[$n] = $row[$n];
+  $types = explode(",", $filters->types);
+  for( $x = 0; $x < count($types); $x++ ){
+    $query ="SELECT id FROM items WHERE type = $types[$x]";
+    if($result = mysqli_query($database, $query)){
+      $id = array();
+      while ($row = mysqli_fetch_row($result)) {
+        for( $n = 0; $n < count($row); $n++ ){
+          return $id[$n] = $row[$n];
+        }
       }
     }
   }
