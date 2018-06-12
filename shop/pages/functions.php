f03604cb505708709c3b21f254b12093f5f8b177
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-12 19:20:30 +0300

    Update functions.php

--- shop/pages/functions.php
+++ shop/pages/functions.php
@@ -1,7 +1,7 @@
 <?
 function get_items($database, $filters){
   $types = explode(",", $filters.types);
-  return var_dump($types);
+  return var_dump($filters.types);
   for( $x = 0; $x < count($types); $x++ ){
     $query ="SELECT id FROM items WHERE type = $types[$x]";
     if($result = mysqli_query($database, $query)){
