f327fb6b3134dd9124c30109a43ca88ef45d6093
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-12 20:31:35 +0300

    Update functions.php

--- shop/pages/functions.php
+++ shop/pages/functions.php
@@ -1,10 +1,10 @@
 <?
 function found_items($database, $filters){
   $types = explode(",", $filters["types"]);
-  $id = array();
   for( $x = 0; $x < count($types); $x++ ){
     $query ="SELECT id FROM items WHERE type = '$types[$x]'";
     if($result = mysqli_query($database, $query)){
+      $id = array();
       while ($row = mysqli_fetch_row($result)) {
         for( $n = 0; $n < count($row); $n++ ){
           $id[$n] = $row[$n];
