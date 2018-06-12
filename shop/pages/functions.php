4f47e82321b8b43cdbff3e362173c30fc11ff976
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-12 20:45:04 +0300

    Update functions.php

--- shop/pages/functions.php
+++ shop/pages/functions.php
@@ -9,7 +9,7 @@ function found_items($database, $filters){
         for( $n = 0; $n < count($row); $n++ ){
           $id[$n] = $row[$n];
         }
-      return var_dump($id[0]); 
+        return var_dump($id); 
       }   
     }else{
       return "Error in:  $query";
