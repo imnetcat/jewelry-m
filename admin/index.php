93770991ce7e7473c7570ef25467172e032648ed
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-12 16:08:05 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html><head>
-  <link rel="stylesheet" href="styles.css" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script async="">
     $( () => {
