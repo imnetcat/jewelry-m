14821f6206d3164d37f8355661260672a5e2370d
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-12 12:02:38 +0300

    Update index.php

--- shop/pages/index.php
+++ shop/pages/index.php
@@ -2,7 +2,8 @@
 <html><head>
   <link rel="stylesheet" href="styles.css" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="filters.js"></script>
+  <script src="filters.js"></script>
+  <script src="post-load.js"></script>
 </head>
 <body>
   <div id="menu">
