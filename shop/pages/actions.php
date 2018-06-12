d295739f36c1af2ba2c7c7f05caf6c51bd1286fe
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-12 13:58:26 +0300

    Update actions.php

--- shop/pages/actions.php
+++ shop/pages/actions.php
@@ -1,11 +1,12 @@
 <?
 require_once "functions.php";
+require_once "../db.php";
 
 
 switch ($_POST['action']){
       case 'get_items':
       $filters = $_POST['filters'];
-      echo get_items($filters);
+      echo get_items($database, $filters);
       break;
 };
 ?>
