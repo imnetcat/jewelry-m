564f2ff137d2cb97f50c66768f765618273a4e43
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-12 16:12:08 +0300

    Update functions.php

--- shop/pages/functions.php
+++ shop/pages/functions.php
@@ -6,7 +6,7 @@ function get_items($database, $filters){
     $id = array();
     while ($row = mysqli_fetch_row($result)) {
       for( $n = 0; n < count($row); $n++ ){
-        $id[$n] = $row[$n];
+        return $id[$n] = $row[$n];
       }
     }
   }
