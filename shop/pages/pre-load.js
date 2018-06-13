953f668fcbfcb7b83bf24e7dc1634b5bb0a88c65
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-13 09:58:54 +0300

    Update pre-load.js

--- shop/pages/pre-load.js
+++ shop/pages/pre-load.js
@@ -114,16 +114,14 @@ function get_items(){
 }
 function php_array_to_js_array(array){
   var splited = array.split('"');
-  //js_array =  '[ '
-  var js_array = "'" + splited[1] + "', ";
+  var js_array = "'" + splited[1] + "',";
   var length = splited.length;
   length -= 2 
   for( n = 3; n < length; n += 2){
-    js_array += "'" + splited[n] + "', ";
+    js_array += "'" + splited[n] + "',";
   }
   js_array += "'" + splited[length] + "'";
-  //js_array += ' ]';
-  return new Array(js_array);
+  return js_array.split(',');
 }
 class Item {
   constructor(array) {
