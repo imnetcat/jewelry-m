d83469a6d3b97eb7cedd5b0b3040f29c65b9d4f2
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-13 23:21:59 +0300

    Update functions.php

--- shop/pages/functions.php
+++ shop/pages/functions.php
@@ -35,21 +35,21 @@ function id_parse($array_of_id){
       }
     }
   }
-  return var_dump($parsed);
+  return $parsed;
 }
 
 function get_items($database, $id){
   for( $x = 0; $x < count($id); $x++ ){
     $query ="SELECT id, image, type, stone, filter_3, filter_4, filter_5, description FROM items WHERE id = '$id[$x]'";
     if($result = mysqli_query($database, $query)){
-      $out = array();
+      //$out = array();
       while ($row = mysqli_fetch_row($result)) {
-        for( $n = 0; $n < count($row); $n++ ){
-          $out[$n] = $row[$n];
-        }
+        return $row;
+        //for( $n = 0; $n < count($row); $n++ ){
+        //  $out[$n] = $row[$n];
+        //}
       }
     }
   }
-  return var_dump($out);
 }
 ?>
