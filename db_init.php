b0696866b82e5509fa42bc1154f702225d1e76f0
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-13 16:21:42 +0300

    Update db_init.php

--- db_init.php
+++ db_init.php
@@ -5,7 +5,7 @@ $query ="CREATE TABLE  IF NOT EXISTS items (
     id INT(20) NOT NULL,
     image VARCHAR(60) NOT NULL,
     type VARCHAR(60) NOT NULL,
-    filter_2 VARCHAR(60) NOT NULL,
+    stone VARCHAR(60) NOT NULL,
     filter_3 VARCHAR(60) NOT NULL,
     filter_4 VARCHAR(60) NOT NULL,
     filter_5 VARCHAR(60) NOT NULL,
