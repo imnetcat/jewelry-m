58fcfe2c5a392c726d09014b854590600daf6031
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-13 16:19:05 +0300

    Update functions.php

--- shop/pages/functions.php
+++ shop/pages/functions.php
@@ -1,13 +1,13 @@
 <?
 function found_items($database, $filters){
-  $id = array();
+  $array_of_id = array();
   $types = explode(",", $filters["types"]);
   $query ="SELECT id, type FROM items";  
   for( $x = 0; $x < count($types); $x++ ){
     if($result = mysqli_query($database, $query)){
       while ($row = mysqli_fetch_row($result)) {
         if($row[1] == $types[$x]){
-          $id['types'][count($id['types'])] = $row[0];
+          $array_of_id['types'][count($id['types'])] = $row[0];
         }
       }
     }  
@@ -18,13 +18,26 @@ function found_items($database, $filters){
     if($result = mysqli_query($database, $query)){
       while ($row = mysqli_fetch_row($result)) {
         if($row[1] == $stones[$x]){
-          $id['stones'][count($id['stones'])] = $row[0];
+          $array_of_id['stones'][count($id['stones'])] = $row[0];
         }
       }
     }  
   }
-  return var_dump($id);
+  return $array_of_id;
 }
+
+function id_parse($array_of_id){
+  $parsed = array();
+  for( $n = 0; $n < count($array_of_id['types']); $n++ ){
+    for( $x = 0; $x < count($array_of_id['stones']); $x++ ){
+      if($array_of_id['types'][$n] == $array_of_id['stones'][$x]){
+        $parsed[count($parsed)] = $array_of_id['types'][$n];
+      }
+    }
+  }
+  return var_dump($parsed);
+}
+
 function get_items($database, $id){
   for( $x = 0; $x < count($id); $x++ ){
     $query ="SELECT id, image, type, filter_2, filter_3, filter_4, filter_5, description FROM items WHERE id = '$id[$x]'";
