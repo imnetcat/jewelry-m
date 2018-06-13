b788a14f15f5794f4038fd7caf25cc03b226cad7
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-13 16:23:01 +0300

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -1,15 +1,15 @@
 <?
 
-function add_new($database, $image, $type, $filter_2, $filter_3, $filter_4, $filter_5, $description){
+function add_new($database, $image, $type, $stone, $filter_3, $filter_4, $filter_5, $description){
   $query = "SELECT MAX(id) FROM items";
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
       $new_id = array_pop($row) + 1;
     }
   }
-  $query = "INSERT INTO items (id, image, type, filter_2, filter_3, filter_4, filter_5, description) VALUES (?,?,?,?,?,?,?,?)";
+  $query = "INSERT INTO items (id, image, type, stone, filter_3, filter_4, filter_5, description) VALUES (?,?,?,?,?,?,?,?)";
   $stmt = mysqli_prepare($database, $query);
-  mysqli_stmt_bind_param($stmt, "isssssss", $new_id, $image, $type, $filter_2, $filter_3, $filter_4, $filter_5, $description);
+  mysqli_stmt_bind_param($stmt, "isssssss", $new_id, $image, $type, $stone, $filter_3, $filter_4, $filter_5, $description);
   if(mysqli_stmt_execute($stmt)){
     mysqli_stmt_close($stmt);
     return "Изображение успешно добавлено!";
