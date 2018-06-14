7867ee105dc179bf82dac013a8b9b8bfa227b2cb
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-14 10:23:02 +0300

    Update pre-load.js

--- shop/pages/pre-load.js
+++ shop/pages/pre-load.js
@@ -107,11 +107,11 @@ function filter_out(){
     },
     success: function(data){
       var raw_data = data.split('array');
-	    console.log(data);
-	    console.log(raw_data);
       var allItems = new Array();
       for( n = 1; n < raw_data.length-1; n++){
-        allItems[n-1] = new Item(php_array_to_js_array(raw_data[n]));
+        allItems[n+1] = new Item(php_array_to_js_array(raw_data[n]));
+        console.log(raw_data.length-1);
+        console.log(raw_data[n]));
         console.log(allItems);
       }
       console.log(allItems);
