698c4d9040698688aad3256dafeccb8cf6eb8ee2
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-14 22:17:34 +0300

    Update functions.js

--- shop/pages/functions.js
+++ shop/pages/functions.js
@@ -77,3 +77,62 @@ function adaptation(){
 	  console.log(oldwidth);
   });
 }
+
+function filter_out(){
+  var raw = $('#types input:checked');
+  var filters = new Object();
+  filters.types = raw[0].id + ',';
+  for(n = 1; n < raw.length; n++){
+    filters.types += raw[n].id + ",";
+  }
+  var raw = $('#stones input:checked');
+  filters.stones = raw[0].id + ',';
+  for(n = 1; n < raw.length; n++){
+    filters.stones += raw[n].id + ",";
+  }
+  console.log(filters.types);
+  console.log(filters.stones);
+  $.ajax({
+    type: "POST",
+    url: "actions.php",
+    data: {
+      action: 'get_items',
+      filters: filters,
+    },
+    success: function(data){
+      var raw_data = data.split('array');
+      var allItems = new Array();
+      for( n = 1; n < raw_data.length; n++){
+        allItems[n-1] = new Item(php_array_to_js_array(raw_data[n]));
+      }
+      console.log(allItems);
+      console.log($('.item'));
+      console.log($('.item').length);
+    }
+  });
+}
+
+function php_array_to_js_array(array){
+  var splited = array.split('"');
+  var js_array = splited[1] + ",";
+  var length = splited.length;
+  length -= 2 
+  for( x = 3; x < length; x += 2){
+    js_array += splited[x] + ",";
+  }
+  js_array += splited[length];
+  return js_array.split(',');
+}
+
+class Item {
+  constructor(array) {
+    this.id = array[0];
+    this.image = array[1];
+    this.type = array[2];
+    this.stone = array[3];
+    this.filter_3 = array[4];
+    this.filter_4 = array[5];
+    this.filter_5 = array[6];
+    this.description = array[7];
+  }
+}
