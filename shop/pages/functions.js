6ba4299598c692af56b2856f81c33a522ffa5bda
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-15 20:55:13 +0300

    Update functions.js

--- shop/pages/functions.js
+++ shop/pages/functions.js
@@ -118,7 +118,8 @@ function filter_out(){
 	  //document.getElementsByClassName('item')[n].src = allItems[n].image;
 	  $('.item:eq('+n+')').attr("src", allItems[n].image);
 	}else{
-          $('.item:eq('+n+')').remove()
+          $('.item:eq('+n+')').remove();
+	}
       }
     }
   });
