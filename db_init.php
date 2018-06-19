983b527fab9f8a87640dc6b8e5b634ff16224080
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-19 16:47:33 +0300

    Add files via upload

---
+++ db_init.php
@@ -0,0 +1,19 @@
+<?
+include "db.php";
+
+$query ="CREATE TABLE  IF NOT EXISTS items (
+    id INT(20) NOT NULL,
+    image VARCHAR(60) NOT NULL,
+    type VARCHAR(60) NOT NULL,
+    stone VARCHAR(60) NOT NULL,
+    filter_3 VARCHAR(60) NOT NULL,
+    filter_4 VARCHAR(60) NOT NULL,
+    filter_5 VARCHAR(60) NOT NULL,
+    description VARCHAR(600) NULL
+)";
+if(mysqli_query($database, $query)){
+}else{
+	echo  mysqli_error($database);
+}
+
+?>
