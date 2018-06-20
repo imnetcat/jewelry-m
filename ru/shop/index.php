b9ac05a34a91737a25a2dcdd4c184d3d7245a18a
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-20 17:20:51 +0300

    Update index.php

--- ru/shop/index.php
+++ ru/shop/index.php
@@ -31,6 +31,8 @@
         <div class="btn left"><img></div>
         <div id="row">
         </div>
+        <div id="description">
+        </div>
         <div class="btn right"><img></div>
       </div>
     </div>
