2845eaafe37534e597ebbaf27b9611c2fb7dd4b9
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-20 13:57:01 +0300

    Update index.php

--- ru/index.php
+++ ru/index.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html><head>
-  <link rel="stylesheet" href="styles.css" type="text/css">
-  <link rel="stylesheet" href="menu.css" type="text/css">
+  <link rel="stylesheet" href="/ru/styles.css" type="text/css">
+  <link rel="stylesheet" href="/ru/menu.css" type="text/css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="pre-load.js"></script>
+  <script src="/ru/pre-load.js"></script>
 </head>
 <body>
   <nav role='navigation' id="menu">
