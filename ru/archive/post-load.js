2b97092c2417b259e74d7db59984e6ce0fee42e8
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-20 12:14:32 +0300

    Add files via upload

---
+++ ru/archive/post-load.js
@@ -0,0 +1,61 @@
+$( () => {
+  $('.fil-close-btn').click( () => {
+    $('.filters').css({
+      visibility: "hidden",
+      zIndex: 99
+    });
+    $('#to_filters').css({
+      visibility: "visible",
+      zIndex: 100
+    });
+    $('#use_filters').css({
+      visibility: "visible",
+      zIndex: 100
+    });
+  });
+
+  $('#to_types').click( () => {
+    $('#types').css({
+      visibility: "visible",
+      zIndex: 100
+    });
+    $('#to_filters').css({
+      visibility: "hidden",
+      zIndex: 99
+    });
+    $('#use_filters').css({
+      visibility: "hidden",
+      zIndex: 99
+    });
+  });
+
+  $('#to_stones').click( () => {
+    $('#stones').css({
+      visibility: "visible",
+      zIndex: 100
+    });
+    $('#to_filters').css({
+      visibility: "hidden",
+      zIndex: 99
+    });
+    $('#use_filters').css({
+      visibility: "hidden",
+      zIndex: 99
+    });
+  });
+  
+  $('#to_technology').click( () => {
+    $('#technology').css({
+      visibility: "visible",
+      zIndex: 100
+    });
+    $('#to_filters').css({
+      visibility: "hidden",
+      zIndex: 99
+    });
+    $('#use_filters').css({
+      visibility: "hidden",
+      zIndex: 99
+    });
+  });
+});
