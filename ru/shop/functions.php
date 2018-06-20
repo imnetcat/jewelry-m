832e65f5abeee5a9d097715dd941b4dc718d4302
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-21 00:56:01 +0300

    Update functions.php

--- ru/shop/functions.php
+++ ru/shop/functions.php
@@ -28,6 +28,22 @@ function found_items($database, $filters){
       }
     }  
   }
+  $technology = explode("-_-", $filters["technology "]);
+  $query ="SELECT id, technology FROM items";  
+  if($result = mysqli_query($database, $query)){
+    while ($row = mysqli_fetch_row($result)) {
+      $item_filters = explode(", ", $row[1]);
+      for($n = 0; $n < count($item_filters); $n++ ){
+        for( $x = 0; $x < count($technology); $x++ ){
+          if($item_filters[$n] == $technology[$x]){
+            if($id['technology'][count($id['technology'])-1] != $row[0]){
+              $id['technology'][count($id['technology'])] = $row[0];
+            }
+          }
+        }
+      }
+    }  
+  }
   return $id;
 }
 
@@ -40,12 +56,19 @@ function id_parse($array_of_id){
       }
     }
   }
+  for( $n = 0; $n < count($parsed); $n++ ){
+    for( $x = 0; $x < count($array_of_id['technology']); $x++ ){
+      if($array_of_id['technology'][$n] == $parsed[$x]){
+        $parsed[count($parsed)] = $array_of_id['technology'][$n];
+      }
+    }
+  }
   return $parsed;
 }
 
 function get_items($database, $id){
   $items = " ";
-  $query ="SELECT id, image, type, stone, filter_3, filter_4, filter_5, description FROM items";
+  $query ="SELECT id, image, type, stone, technology, filter_4, filter_5, description FROM items";
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
       for( $x = 0; $x < count($id); $x++ ){
