142a1f2e007113643f7bcdcdb62f04b6b96d376f
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-21 19:17:42 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -25,7 +25,7 @@
 	    type: $('#new_type').val(),
 	    stone: $('#new_stone').val(),
 	    technology: $('#new_technology').val(),
-	    filter_4: $('#new_cost').val(),
+	    cost: $('#new_cost').val(),
 	    filter_5: $('#new_filter_5').val(),
 	    description: $('#new_description').val()
 	  },
