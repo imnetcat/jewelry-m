5cc9474d556c79154fc4dfb8c050a8ab1eeb5333
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-21 19:17:06 +0300

    Update actions.php

--- admin/actions.php
+++ admin/actions.php
@@ -10,10 +10,10 @@ switch ($_POST['action']){
     $type = $_POST['type'];
     $stone = $_POST['stone'];
     $technology = $_POST['technology'];
-    $filter_4 = $_POST['filter_4'];
+    $cost = $_POST['cost'];
     $filter_5 = $_POST['filter_5'];
     $description = $_POST['description'];
-    echo add_new($database, $image, $type, $stone, $technology, $filter_4, $filter_5, $description);
+    echo add_new($database, $image, $type, $stone, $technology, $cost, $filter_5, $description);
   break;
   case 'delete':
     $id = $_POST['id'];
