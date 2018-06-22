bf822e00ce2a37ec463b1e30e121e48e7ec1eac0
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-06-22 20:20:17 +0300

    Update functions.php

--- ru/shop/functions.php
+++ ru/shop/functions.php
@@ -74,7 +74,6 @@ function get_items($database, $id, $sortings){
   }else{
     $sort = "cost DESC";
   }
-  return $sort;
   $raw = array();
   $query ="SELECT id, image, type, stone, technology, cost, filter_5, description FROM items ORDER BY " . $sort;
   if($result = mysqli_query($database, $query)){
