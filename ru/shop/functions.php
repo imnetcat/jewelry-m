70cd469a1374238fa534698eaef34ffa6cb32f98
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-08-18 00:28:44 +0300

    Update functions.php

--- ru/shop/functions.php
+++ ru/shop/functions.php
@@ -83,10 +83,11 @@ function get_items($database, $id, $sortings){
   }
   $items = " ";
   for( $n = 0; $n < count($id); $n++ ){
-    return ( 0 < count($raw));
     for( $x = 0; $x < count($raw); $x++ ){
       if($raw[$n][0] == $id[$x]){
         $items .= var_dump($raw[$n]);
+      }else{
+        $items .= "+/";
       }
     }
   }
