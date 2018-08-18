2ea61c70ae025c14bc502b64c60188aee67455bd
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-08-18 12:10:30 +0300

    Update functions.php

--- admin/functions.php
+++ admin/functions.php
@@ -1,13 +1,13 @@
 <?
 
-function add_new($database, $image, $type, $stone, $technology, $cost, $filter_5, $description){
-  $query = "SELECT MAX(id) FROM items";
+function add_in_shop($database, $image, $type, $stone, $technology, $cost, $filter_5, $description){
+  $query = "SELECT MAX(id) FROM shop";
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
       $new_id = array_pop($row) + 1;
     }
   }
-  $query = "INSERT INTO items (id, image, type, stone, technology, cost, filter_5, description) VALUES (?,?,?,?,?,?,?,?)";
+  $query = "INSERT INTO shop (id, image, type, stone, technology, cost, filter_5, description) VALUES (?,?,?,?,?,?,?,?)";
   $stmt = mysqli_prepare($database, $query);
   mysqli_stmt_bind_param($stmt, "isssssss", $new_id, $image, $type, $stone, $technology, $cost, $filter_5, $description);
   if(mysqli_stmt_execute($stmt)){
@@ -26,4 +26,16 @@ function delete($database, $id){
     return "Error in: " . $query . "<br>" . mysqli_error($database);
   }
 }
+
+function get_shop($database){
+  $items = " ";
+  $query ="SELECT id, image, type, stone, technology, cost, filter_5, description  FROM shop";
+  if($result = mysqli_query($database, $query)){
+    while ($row = mysqli_fetch_row($result)) {
+      $items .= var_dump($row);
+    }
+  }
+  return $items;
+}
+
 ?>
