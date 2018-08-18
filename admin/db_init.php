904641a2e68faab1b109b8be1f0022f003845769
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-08-18 13:46:51 +0300

    Update db_init.php

--- admin/db_init.php
+++ admin/db_init.php
@@ -1,19 +1,33 @@
 <?
 include "db.php";
 
-$query ="CREATE TABLE  IF NOT EXISTS items (
-    id INT(20) NOT NULL,
-    image VARCHAR(60) NOT NULL,
-    type VARCHAR(60) NOT NULL,
-    stone VARCHAR(60) NOT NULL,
-    technology VARCHAR(60) NOT NULL,
-    cost VARCHAR(60) NOT NULL,
-    filter_5 VARCHAR(60) NOT NULL,
-    description VARCHAR(600) NULL
+$query ="CREATE TABLE  IF NOT EXISTS shop (
+  id INT(20) NOT NULL,
+  image VARCHAR(60) NOT NULL,
+  type VARCHAR(60) NOT NULL,
+  stone VARCHAR(60) NOT NULL,
+  technology VARCHAR(60) NOT NULL,
+  cost VARCHAR(60) NOT NULL,
+  filter_5 VARCHAR(60) NOT NULL,
+  description VARCHAR(600) NULL
 )";
 if(mysqli_query($database, $query)){
 }else{
-	echo  mysqli_error($database);
+  echo  mysqli_error($database);
+}
+$query ="CREATE TABLE  IF NOT EXISTS archive (
+  id INT(20) NOT NULL,
+  image VARCHAR(60) NOT NULL,
+  type VARCHAR(60) NOT NULL,
+  stone VARCHAR(60) NOT NULL,
+  technology VARCHAR(60) NOT NULL,
+  cost VARCHAR(60) NOT NULL,
+  filter_5 VARCHAR(60) NOT NULL,
+  description VARCHAR(600) NULL
+)";
+if(mysqli_query($database, $query)){
+}else{
+  echo  mysqli_error($database);
 }
 
 ?>
