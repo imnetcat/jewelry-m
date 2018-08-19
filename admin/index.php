8db741ccc964299d180d7c5182a059554a9e5234
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-08-19 12:03:58 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -116,81 +116,6 @@
         });
       });
 	    
-      $('.del').click( () => {
-	      console.log(this.parent());
-	      console.log(this.parent().parent());
-	      console.log(this.parent().parent().find("span[class='id']"));
-	      console.log(this.parent().parent().find("span[class='id']")[0]);
-	      console.log(this.parent().parent().find("span[class='id']")[0].text());
-	var delID = this.parent().parent().find("span[class='id']")[0].text();
-	var derectory = this.parent().parent().find("span[class='image']")[0].text().split("/")[1];
-	      console.log(derectory);
-	$.ajax({
-          type: "POST",
-          url: "actions.php",
-          data: {
-            action: 'delete',
-            id: delID,
-            derectory: derectory
-          },
-          success: function(data){
-            $('#info').html($('#info').html() + "<br>" + data);
-          }
-        });
-      });
-      
-      $('.change').click( () => {
-	      console.log(this.parent());
-	      console.log(this.parent().parent());
-	      console.log(this.parent().parent().find("span[class='id']"));
-	      console.log(this.parent().parent().find("span[class='id']")[0]);
-	      console.log(this.parent().parent().find("span[class='id']")[0].text());
-	var delID = this.parent().parent().find("span[class='id']")[0].text();
-	var derectory = this.parent().parent().find("span[class='image']")[0].text().split("/")[1];
-	      console.log(derectory);
-	$.ajax({
-          type: "POST",
-          url: "actions.php",
-          data: {
-            action: 'change',
-            id: delID,
-            derectory: derectory,
-            action: 'add_in_archive',
-	    image: "items/"+$('#new_image').val(),
-	    type: $('#new_type').val(),
-	    stone: $('#new_stone').val(),
-	    technology: $('#new_technology').val(),
-	    cost: cost,
-	    filter_5: $('#new_filter_5').val(),
-	    description: $('#new_description').val()
-          },
-          success: function(data){
-            $('#info').html($('#info').html() + "<br>" + data);
-          }
-        });
-      });	    
-      
-      $('.copy').click( () => {
-	      console.log(this.parent());
-	      console.log(this.parent().parent());
-	      console.log(this.parent().parent().find("span[class='id']"));
-	      console.log(this.parent().parent().find("span[class='id']")[0]);
-	      console.log(this.parent().parent().find("span[class='id']")[0].text());
-	var image = this.parent().parent().find("span[class='image']")[0].text();
-	var type = this.parent().parent().find("span[class='type']")[0].text();
-	var stone = this.parent().parent().find("span[class='stone']")[0].text();
-	var technology = this.parent().parent().find("span[class='technology']")[0].text();
-	var cost = this.parent().parent().find("span[class='cost']")[0].text();
-	var filter_5 = this.parent().parent().find("span[class='filter_5']")[0].text();
-	var description = this.parent().parent().find("span[class='description']")[0].text();
-	$('#new_image').text(image);
-	$('#new_stone').text(stone);
-	$('#new_technology').text(technology);
-	$('#new_cost').text(cost);
-	$('#new_filter_5').text(filter_5);
-	$('#new_description').text(description);
-      });
-	    
       $('#get_shop').click( () => {
         $('.item').remove();
 	$.ajax({
@@ -222,6 +147,74 @@
 	      var c = $("<div class='c' style='margin-left:900px'><br><button class='del'>Удалить</button><br><br><button class='change'>Изменить</button><br><br><button class='copy'>Скопировать</button></div>");
 	      div.append(c);
 	      $('#container').append(div);
+	      $('.del').click( () => {
+	        console.log(this.parent());
+	        console.log(this.parent().parent());
+	        console.log(this.parent().parent().find("span[class='id']"));
+	        console.log(this.parent().parent().find("span[class='id']")[0]);
+	        console.log(this.parent().parent().find("span[class='id']")[0].text());
+	        var delID = this.parent().parent().find("span[class='id']")[0].text();
+	        $.ajax({
+                  type: "POST",
+                  url: "actions.php",
+                  data: {
+                    action: 'delete',
+                    id: delID,
+                    derectory: "shop"
+                  },
+                  success: function(data){
+                    $('#info').html($('#info').html() + "<br>" + data);
+                  }
+                });
+              });
+              $('.change').click( () => {
+	        console.log(this.parent());
+	        console.log(this.parent().parent());
+	        console.log(this.parent().parent().find("span[class='id']"));
+	        console.log(this.parent().parent().find("span[class='id']")[0]);
+	        console.log(this.parent().parent().find("span[class='id']")[0].text());
+	        var ID = this.parent().parent().find("span[class='id']")[0].text();
+	        $.ajax({
+                  type: "POST",
+                  url: "actions.php",
+                  data: {
+                    action: 'change',
+                    id: ID,
+                    derectory: "shop",
+                    action: 'add_in_archive',
+	            image: "items/"+$('#new_image').val(),
+	            type: $('#new_type').val(),
+	            stone: $('#new_stone').val(),
+	            technology: $('#new_technology').val(),
+	            cost: cost,
+	            filter_5: $('#new_filter_5').val(),
+	            description: $('#new_description').val()
+                  },
+                  success: function(data){
+                    $('#info').html($('#info').html() + "<br>" + data);
+                  }
+                });
+              });
+              $('.copy').click( () => {
+	        console.log(this.parent());
+	        console.log(this.parent().parent());
+	        console.log(this.parent().parent().find("span[class='id']"));
+	        console.log(this.parent().parent().find("span[class='id']")[0]);
+	        console.log(this.parent().parent().find("span[class='id']")[0].text());
+	        var image = this.parent().parent().find("span[class='image']")[0].text();
+	        var type = this.parent().parent().find("span[class='type']")[0].text();
+	        var stone = this.parent().parent().find("span[class='stone']")[0].text();
+	        var technology = this.parent().parent().find("span[class='technology']")[0].text();
+	        var cost = this.parent().parent().find("span[class='cost']")[0].text();
+	        var filter_5 = this.parent().parent().find("span[class='filter_5']")[0].text();
+	        var description = this.parent().parent().find("span[class='description']")[0].text();
+	        $('#new_image').text(image);
+	        $('#new_stone').text(stone);
+	        $('#new_technology').text(technology);
+	        $('#new_cost').text(cost);
+	        $('#new_filter_5').text(filter_5);
+	        $('#new_description').text(description);
+              });
             }
           }
         });
