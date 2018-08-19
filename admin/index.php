8d2e79f048957c0d2fcb354cd903cd473dc5a043
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-08-19 16:48:10 +0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -196,13 +196,13 @@
                 });
               });
               $('.copy').on('click', (ev) => {
-	        var image = $(ev.currentTarget).closest('.item').find('.a').find(".image").text();
-	        var type = $(ev.currentTarget).closest('.item').find('.a').find(".type").text();
-	        var stone = $(ev.currentTarget).closest('.item').find('.a').find(".stone").text();
-	        var technology = $(ev.currentTarget).closest('.item').find('.a').find(".technology").text();
-	        var cost = $(ev.currentTarget).closest('.item').find('.a').find(".cost").text();
-	        var filter_5 = $(ev.currentTarget).closest('.item').find('.a').find(".filter_5").text();
-	        var description = $(ev.currentTarget).closest('.item').find('.a').find(".description").text();
+	        var image = $(ev.currentTarget).parent().parent().find('.a').find(".image").text();
+	        var type = $(ev.currentTarget).parent().parent().find('.a').find(".type").text();
+	        var stone = $(ev.currentTarget).parent().parent().find('.a').find(".stone").text();
+	        var technology = $(ev.currentTarget).parent().parent().find('.a').find(".technology").text();
+	        var cost = $(ev.currentTarget).parent().parent().find('.a').find(".cost").text();
+	        var filter_5 = $(ev.currentTarget).parent().parent().find('.a').find(".filter_5").text();
+	        var description = $(ev.currentTarget).parent().parent().find('.a').find(".description").text();
 		console.log(image);
 		console.log(type);
 		console.log(stone);
