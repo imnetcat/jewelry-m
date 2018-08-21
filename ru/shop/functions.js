fcfd8897c7d1a831bedebed660fb7965b001b55d
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-08-21 11:43:20 +0300

    Update functions.js

--- ru/shop/functions.js
+++ ru/shop/functions.js
@@ -1,5 +1,5 @@
 function build_item(){
-  return '<div><img class="item" src="items/default.png"><div class="description"></div></div>';
+  return '<div><img class="item" src="items/default.png"><div class="description"><div class="more"></div></div></div>';
 }
 function adaptation_1(){
   var client_w = screen.width;
