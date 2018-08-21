c5b48a1e31ba7c189d94262e26a104a0c08c275b
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-08-21 09:27:51 +0300

    Update functions.php

--- ru/archive/functions.php
+++ ru/archive/functions.php
@@ -2,7 +2,7 @@
 function found_items($database, $filters){
   $id = array();
   $types = explode("-_-", $filters["types"]);
-  $query ="SELECT id, type FROM archive";  
+  $query ="SELECT id, type FROM archive";
   for( $x = 0; $x < count($types); $x++ ){
     if($result = mysqli_query($database, $query)){
       while ($row = mysqli_fetch_row($result)) {
@@ -13,7 +13,7 @@ function found_items($database, $filters){
     }  
   }
   $stones = explode("-_-", $filters["stones"]);
-  $query ="SELECT id, stone FROM archive";  
+  $query ="SELECT id, stone FROM archive";
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
       $item_filters = explode(", ", $row[1]);
@@ -22,6 +22,22 @@ function found_items($database, $filters){
           if($item_filters[$n] == $stones[$x]){
             if($id['stones'][count($id['stones'])-1] != $row[0]){
               $id['stones'][count($id['stones'])] = $row[0];
+            }
+          }
+        }
+      }
+    }  
+  }
+  $technology = explode("-_-", $filters["technology"]);
+  $query ="SELECT id, technology FROM archive";  
+  if($result = mysqli_query($database, $query)){
+    while ($row = mysqli_fetch_row($result)) {
+      $item_filters = explode(", ", $row[1]);
+      for($n = 0; $n < count($item_filters); $n++ ){
+        for( $x = 0; $x < count($technology); $x++ ){
+          if($item_filters[$n] == $technology[$x]){
+            if($id['technology'][count($id['technology'])-1] != $row[0]){
+              $id['technology'][count($id['technology'])] = $row[0];
             }
           }
         }
@@ -40,17 +56,32 @@ function id_parse($array_of_id){
       }
     }
   }
+  for( $n = 0; $n < count($parsed); $n++ ){
+    for( $x = 0; $x < count($array_of_id['technology']); $x++ ){
+      if($parsed[$n] == $array_of_id['technology'][$x]){
+        $parsed__[count($parsed__)] = $parsed[$n];
+      }
+    }
+  }
+  $parsed = $parsed__;
+  $parsed__ = array();
   return $parsed;
 }
 
-function get_items($database, $id){
+function get_items($database, $id, $sortings){
+  if($sortings['cost'] == "cost-down-to-up"){
+    $sort = "cost";
+  }else{
+    $sort = "cost DESC";
+  }
+  $raw = array();
   $items = " ";
-  $query ="SELECT id, image, type, stone, filter_3, filter_4, filter_5, description FROM archive";
+  $query ="SELECT id, image, type, stone, technology, cost, filter_5, description FROM archive ORDER BY " . $sort;
   if($result = mysqli_query($database, $query)){
     while ($row = mysqli_fetch_row($result)) {
-      for( $x = 0; $x < count($id); $x++ ){
-        if($row[0] == $id[$x]){
-         $items .= var_dump($row);
+      for( $n = 0; $n < count($id); $n++ ){
+        if($row[0] == $id[$n]){
+          $items .= var_dump($row);
         }
       }
     }
