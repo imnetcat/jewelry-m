aa144ce84c535434deb06c3787e6b0553f38837e
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-08-21 22:27:15 +0300

    Update index.php

--- ru/shop/index.php
+++ ru/shop/index.php
@@ -26,7 +26,7 @@
       <li><a href="/ru/contacts/">Контакты</a></li>
     </ul>
   </nav>
-  <div id="currentItem"></div>
+  <div id="currentItem"><div id='currentImg'></div></div>
   <div id="container">
     <div id="items">
       <div class="row">
