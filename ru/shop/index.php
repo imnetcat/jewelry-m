6e72fb5754ab3243558b3180b528e8c114f41b85
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-08-25 00:43:10 +0300

    Update index.php

--- ru/shop/index.php
+++ ru/shop/index.php
@@ -47,6 +47,23 @@
       <div class="use_filters">сортировать</div>
     </div>
   </div>
+  <div id="fullImg">
+    <img src=""/>
+    <div id="floatingCirclesG">
+      <div class="f_circleG" id="frotateG_01"></div>
+      <div class="f_circleG" id="frotateG_02"></div>
+      <div class="f_circleG" id="frotateG_03"></div>
+      <div class="f_circleG" id="frotateG_04"></div>
+      <div class="f_circleG" id="frotateG_05"></div>
+      <div class="f_circleG" id="frotateG_06"></div>
+      <div class="f_circleG" id="frotateG_07"></div>
+      <div class="f_circleG" id="frotateG_08"></div>
+    </div>
+    <div class="fil-close-btn" style="top: 80vh; left: 49%;">
+      <div></div>
+      <div></div>
+    </div>
+  </div>
   <footer>
   </footer>
 </body>
