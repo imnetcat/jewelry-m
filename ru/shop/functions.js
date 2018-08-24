cf9b84aea5a0688e9de50a9375fe416ea5c618a8
imnetcat <37350375+imnetcat@users.noreply.github.com>
2018-08-25 00:45:09 +0300

    Update functions.js

--- ru/shop/functions.js
+++ ru/shop/functions.js
@@ -265,7 +265,7 @@ function set_pages(allItems){
       n++; 
     }
     $('#currentImg').click( () => {
-      $('body').append('<div id="fullImg"><img src="items/full/'+$('#currentImg').attr('src').split("/")[1]+'"/><div id="floatingCirclesG"><div class="f_circleG" id="frotateG_01"></div><div class="f_circleG" id="frotateG_02"></div><div class="f_circleG" id="frotateG_03"></div><div class="f_circleG" id="frotateG_04"></div><div class="f_circleG" id="frotateG_05"></div><div class="f_circleG" id="frotateG_06"></div><div class="f_circleG" id="frotateG_07"></div><div class="f_circleG" id="frotateG_08"></div></div><div class="fil-close-btn" style="top: 80vh; left: 49%;"><div></div><div></div></div></div>');
+      $('#fullImg > img').attr("src", "items/full/"+$('#currentImg').attr('src').split('/')[1]);
       $('#currentItem .fil-close-btn').click( () => {
         $('#currentItem').css('visibility', 'hidden');
       });
